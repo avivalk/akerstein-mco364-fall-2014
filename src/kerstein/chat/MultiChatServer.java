9c7a5fcd2f057f448b5a09ebd607133a08749b14
avivalk <aviva.kerstein@huffingtonpost.com>
2014-09-30 22:32:55 -0400

    MulitThreading Servers

---
+++ src/kerstein/chat/MultiChatServer.java
@@ -0,0 +1,28 @@
+package kerstein.chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MultiChatServer {
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(9097);
+	   BlockingQueue<String>messages=new LinkedBlockingQueue<String>();
+	   List<Socket>sockets=new ArrayList<Socket>();
+		MessageSender sender=new MessageSender(messages, sockets);
+		sender.start();
+		
+		while (true) {
+			Socket socket = serverSocket.accept();
+			sockets.add(socket);
+			SocketHandler handler = new SocketHandler( socket, messages);
+			handler.start();
+		}
+	}
+
+}
---
+++ src/kerstein/chat/SocketHandler.java
@@ -0,0 +1,31 @@
+package kerstein.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Queue;
+
+public class SocketHandler extends Thread {
+   private Socket socket;
+   private Queue<String>messages;
+   
+   public SocketHandler(Socket socket,Queue<String>messages){
+	   this.socket=socket;
+	   this.messages=messages;
+   }
+   public void run() {
+
+		InputStream in;
+		try {
+			in = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String line;
+			while ((line=reader.readLine())!=null) {
+}                 messages.add(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+   }
+}
