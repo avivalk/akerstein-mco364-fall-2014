8ef0e4e4919365163b4e17ccabc5f68ff8370318
avivalk <avivakerstein@gmail.com>
2014-12-21 19:02:36 -0500

    did layers

--- src/kerstein/paint/Canvas.java
+++ src/kerstein/paint/Canvas.java
@@ -1,5 +1,6 @@
 package kerstein.paint;
 
+import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -13,25 +14,25 @@ public class Canvas extends JComponent {
 	private int strokeWidth;
 	private DrawListener listener;
 	private BufferedImage[] layers;
-	private int layeredSelected;
+	private int layerSelected;
 
 	public Canvas() {
-		layers=new BufferedImage[4];
-		for(int i=0; i<4; i++){
-			layers[i]=new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
-		}
+		layers = new BufferedImage[4];
 		setLayerSelected(0);
-		Graphics2D g=(Graphics2D) layers[0].getGraphics();
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 800, 600);
-		this.color=Color.BLACK;
+		for (int i = 0; i < 4; i++) {
+			layers[i] = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+			clear(i);
+		}
+		this.color = Color.BLACK;
 		setDrawListener(new PencilListener(this));
 	}
-	public void setLayerSelected(int layeredSelected){
-		this.layeredSelected=layeredSelected;
+
+	public void setLayerSelected(int layeredSelected) {
+		this.layerSelected = layeredSelected;
 	}
-	public int getLayeredSelected(){
-		return layeredSelected;
+
+	public int getLayerSelected() {
+		return layerSelected;
 	}
 
 	public void setDrawListener(DrawListener dl) {
@@ -41,7 +42,8 @@ public class Canvas extends JComponent {
 		this.addMouseListener(dl);
 		this.listener = dl;
 	}
-	public BufferedImage[] getLayers(){
+
+	public BufferedImage[] getLayers() {
 		return layers;
 	}
 
@@ -50,14 +52,13 @@ public class Canvas extends JComponent {
 	}
 
 	public Graphics2D getGraphicsPen() {
-		BufferedImage[]images=getLayers();
-		BufferedImage selected=getLayers()[getLayeredSelected()];
-		Graphics2D gr=(Graphics2D) selected.getGraphics();
+		BufferedImage[] images = getLayers();
+		BufferedImage selected = getLayers()[getLayerSelected()];
+		Graphics2D gr = (Graphics2D) selected.getGraphics();
 		gr.setColor(this.color);
 		return gr;
 
 	}
-	
 
 	public void setStrokeWidth(int width) {
 		this.strokeWidth = width;
@@ -74,21 +75,31 @@ public class Canvas extends JComponent {
 		this.color = color;
 	}
 
-	
+	public void clear(int layer) {
+		if (layer == 0) {
+			Graphics2D g = (Graphics2D) layers[0].getGraphics();
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, 800, 600);
+			g.setColor(this.color);
+		} else {
 
-	public void clear() {
-		Graphics2D g = (Graphics2D) getLayers()[getLayeredSelected()].getGraphics();
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 800, 600);
-		g.setColor(this.color);
+			layers[layer] = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D graphics = (Graphics2D) layers[layer].getGraphics();
+			graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+			graphics.fillRect(0, 0, 800, 600);
+			// reset composite
+			graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+			graphics.setColor(this.color);
+		}
+
 		repaint();
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		for(int i=0; i<4; i++){
-		g.drawImage(layers[i], 0, 0, null);
+		for (int i = 0; i < 4; i++) {
+			g.drawImage(layers[i], 0, 0, null);
 		}
 		g.setColor(this.color);
 		setStrokeWidth(this.strokeWidth);
